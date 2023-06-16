5fae93fc83510230ce05df9a06af907307896c9b
Wei <kiwi072400.lin@gmail.com>
2023-06-16 23:07:39 +0800

    增加註釋

--- src/game.c
+++ src/game.c
@@ -2,45 +2,54 @@
 #include <stdlib.h>
 #include <time.h>
 
+// 遊戲設定
 #define MAP_SIZE 20
 #define MAX_EVENT 5
 
+// 訊息
 #define SYSTEM_MESSAGE "[系統訊息]"
 #define PLAYER_MESSAGE "[玩家訊息]"
 
+// 玩家
 typedef struct Player
 {
     int position;
     int money;
 } Player;
 
+// 事件
 typedef struct Event
 {
     const char *description;
     int moneyChange;
 } Event;
 
+// 事件列表
 Event events[MAX_EVENT] = {
     {"你找到了一筆錢，獲得$100!", 100}, {"你被罰款$50!", -50}, {"你被罰款$100!", -100},
     {"你找到了一筆錢，獲得$50!", 50},   {"沒有事件發生", 0},
 };
 
+// 初始化玩家
 void initializePlayer(Player *player)
 {
     player->position = 0;
     player->money = 1000;
 }
 
+// 擲骰子
 int rollDice()
 {
     return rand() % 6 + 1;
 }
 
+// 隨機取得事件
 Event getEvent()
 {
     return events[rand() % MAX_EVENT];
 }
 
+// 主程式
 int main()
 {
     printf("這場遊戲有%d個區塊\n", MAP_SIZE);
